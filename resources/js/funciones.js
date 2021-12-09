3bef240b5076039558c32ad520a2f44733184455
Ariel Rosa <34896836+ariel0000@users.noreply.github.com>
2021-12-09 13:23:51 -0300

    Agregado archivo de funciones para WebSocket
    Dentro de esta archivo de Javascript se encuentran los métodos necesarios para establecer una comunicación con WebSocket y poder manejar un chat dentro de la App

---
+++ resources/js/funciones.js
@@ -0,0 +1,50 @@
+function enviarTexto(event){event.preventDefault();
+    var campo=event.target.texto;
+    doSend(campo.value);
+    campo.value="";
+}
+
+function init() {
+    wsConnect();
+}
+
+function wsConnect() {
+    websocket= new WebSocket("ws://localhost:3000");
+
+    websocket.onopen = function (evt) {
+        onOpen(evt)
+    };websocket.onclose = function (evt) {
+        onClose(evt)
+    };websocket.onmessage = function (evt) {
+        onMessage(evt)
+    };websocket.onerror = function (evt) {
+        onError(evt)
+    };
+}
+
+function onOpen(evt) {
+    document.getElementById("enviar").disabled= false;
+    doSend("Hola");
+}
+
+function onClose(evt) {
+
+    document.getElementById("enviar").disabled= true;
+
+    setTimeout(function () {
+        wsConnect()
+    }, 2000);
+}
+
+function onMessage(evt) {
+    var area=document.getElementById("mensajes")
+    area.innerHTML+=evt.data+ "\n";
+}
+
+function onError(evt) {console.log("ERROR: " +evt.data);
+}
+
+function doSend(message) {console.log("Enviando: " +message);websocket.send(message);
+}
+
+window.addEventListener("load",init, false);
